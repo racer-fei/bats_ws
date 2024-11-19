45b7f5aa3ddd5f55ef13f8516b241d2ec0610e52
MetroDood <icrocelysium@hotmail.com>
2024-11-19 14:37:24 -0300

    Update esp_motors.py
    Now with Pinout and command list;
    Also serial communication

--- esp_motors.py
+++ esp_motors.py
@@ -1,33 +1,76 @@
-global MAX_SPEED
-MAX_SPEED = 50 #placeholder
-
-global turn90
-turn90 = 500 #placeholder
-
+import machine
 import time
+import uasyncio as asyncio
 
-def fullSpeed():
-    motor_left = MAX_SPEED
-    motor_right = MAX_SPEED
+# GPIO da ESP32
+MOTOR_LEFT_PIN1 = machine.Pin(12, machine.Pin.OUT) 
+MOTOR_LEFT_PIN2 = machine.Pin(13, machine.Pin.OUT)
+MOTOR_RIGHT_PIN1 = machine.Pin(2, machine.Pin.OUT)
+MOTOR_RIGHT_PIN2 = machine.Pin(15, machine.Pin.OUT)
+BACK_MOTOR_PIN = machine.Pin(6, machine.Pin.OUT)
 
-def bodySpin():
-    motor_left = MAX_SPEED
-    motor_right = -MAX_SPEED
-    time.sleep(turn90)
+#commands seriais da Rasp
+async def commandList(command):
+    if command == 'L':
+        turnLeft()
+    elif command == 'R':
+        turnRight()
+    elif command == 'F':
+        goForward()
+    elif command == 'B':
+        goBackward()
+    elif command == 'O':
+        openBack()
+    elif command == 'C':
+        closeBack()
+    else:
+        stop_all()
 
-def collectObjective(): #placeholder
-    servo = MAX_SPEED
-    time.sleep(10)
-    motor_left = MAX_SPEED
-    motor_right = MAX_SPEED
-    time.sleep(20)
-    servo = -MAX_SPEED
-    bodySpin()
+def turnLeft():
+    MOTOR_LEFT_PIN1.value(0)
+    MOTOR_LEFT_PIN2.value(1)
+    MOTOR_RIGHT_PIN1.value(1)
+    MOTOR_RIGHT_PIN2.value(0)
 
-def turnLeft(strength):
-    motor_left = motor_left - strength
+def turnRight():
+    MOTOR_LEFT_PIN1.value(1)
+    MOTOR_LEFT_PIN2.value(0)
+    MOTOR_RIGHT_PIN1.value(0)
+    MOTOR_RIGHT_PIN2.value(1)
 
-def turnRight(strength):
-    motor_right = motor_right - strength
+def goForward():
+    MOTOR_LEFT_PIN1.value(1)
+    MOTOR_LEFT_PIN2.value(0)
+    MOTOR_RIGHT_PIN1.value(1)
+    MOTOR_RIGHT_PIN2.value(0)
 
+def goBackward():
+    MOTOR_LEFT_PIN1.value(0)
+    MOTOR_LEFT_PIN2.value(1)
+    MOTOR_RIGHT_PIN1.value(0)
+    MOTOR_RIGHT_PIN2.value(1)
 
+def openBack():
+    BACK_MOTOR_PIN.value(1)  
+
+def closeBack():
+    BACK_MOTOR_PIN.value(0)
+
+def stop_all():
+    MOTOR_LEFT_PIN1.value(0)
+    MOTOR_LEFT_PIN2.value(0)
+    MOTOR_RIGHT_PIN1.value(0)
+    MOTOR_RIGHT_PIN2.value(0)
+
+#Comunicação serial entre RASP e Esp32; Tx - Transfere/Transfer, Rx - Recebe/Read
+uart = machine.UART(1, baudrate=9600, tx=17, rx=16)
+
+async def main():
+    while True:
+        if uart.any():
+            command = uart.read(1).decode('utf-8')
+            await commandList(command)
+        await asyncio.sleep(0.1)
+
+#ativa o loop principal de leituras
+asyncio.run(main())
